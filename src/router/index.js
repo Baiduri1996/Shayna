cd9501a4dedd0e5237e6c36f624449d7c807d701
Baiduri <mbaiduri1@gmail.com>
2020-09-01 07:55:43 -0700

    done convert rest pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Product from '../views/Product.vue';
+import ShoppingCart from '../views/ShoppingCart.vue';
+import Success from '../views/Success.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: Product,
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: ShoppingCart,
+  },
+  {
+    path: '/success',
+    name: 'success',
+    component: Success,
   },
 ];
 
