fbca9ecfe534ba65423a1872442ff929eda6966c
Baiduri <mbaiduri1@gmail.com>
2020-09-01 03:38:34 -0700

    new

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <headerShayna />
+    <HeroShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+  },
+};
+</script>
